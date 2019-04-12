4ffa4f5a0ab1174c62f5c244536edfc78294dc46
SamuelDeboni <samueldeboniss@gmail.com>
2019-04-12 15:43:16 -0300

    added prototype level gen

--- LevelGenPrototype.gd
+++ LevelGenPrototype.gd
@@ -6,33 +6,35 @@ func generate_level():
 				 [0,0,0,0],
 				 [0,0,0,0]]
 	
-	var map_seed = 846844
+	var map_seed = 2624895
 	seed(map_seed)
 	
 	var gx = 0
 	var gy = 0
 	level[gx][gy] = 1
+	var dir = int(rand_range(0,2))
 	
 	for i in range(0,32):
 		
 		seed(randi())
-		var dir = int(rand_range(0,2))
 		
-		var can_d = gy+1 < 3
+		var can_d = gy+1 <= 3
 		var can_l = gx-1 >= 0
 		var can_r = gx+1 <= 3
 		var cant_sides = (dir == 0 and not can_r) or (dir == 1 and not can_l)
 		
-		if can_d and (randf() > 0.7 or cant_sides):
+		if can_d and (randf() > 0.9 or cant_sides):
 			gy += 1	
-			dir = int(rand_range(0,2))
+			var r = int(rand_range(0,2))
+			dir = r
+			print(r)
 		else:
 			if dir == 0 and can_r:
 				gx += 1
 			elif dir == 1 and can_l:
 				gx -= 1
 		
-		if gy == 3 and cant_sides:
+		if not can_d and cant_sides:
 			break
 		
 		level[gx][gy] = 2
