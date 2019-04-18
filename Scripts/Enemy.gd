4b027079848fea0ca480caab19f4f22d6f003490
SamuelDeboni <samueldeboniss@gmail.com>
2019-04-18 14:42:38 -0300

    Enemies changes

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -2,6 +2,8 @@ extends PathFollow2D
 
 var vel = 100
 
+export var helth = 100
+
 func _ready():
 	loop = true
 	rotate = false
@@ -10,6 +12,15 @@ func _ready():
 func _process(delta):
 	var old_position = position
 	offset += delta * vel
-	
 	$Sprite.flip_h = (position - old_position).x > 0
+	if $Timer.time_left == 0:
+		$Sprite.modulate = Color(1,1,1)
 	
+	if Input.is_action_just_pressed("ui_down"):
+		damage(1)
+
+func damage(var damage: float):
+	damage -= damage
+	$Sprite.modulate = Color(1,0,0)
+	$Timer.start()
+		
